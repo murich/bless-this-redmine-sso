a5dc2ebdbab751e76e41b98bdf7e235af1f9b947
murich <murich@users.noreply.github.com>
2025-10-08 16:32:11 +0300

    refactor: remove redundant flash messages, redirect to account page
    - Remove flash error messages (notice already shown on /my/account page)
    - Redirect to /my/account instead of external Casdoor URL

--- lib/bless_this_redmine_sso/patches/my_controller_patch.rb
+++ lib/bless_this_redmine_sso/patches/my_controller_patch.rb
@@ -14,22 +14,14 @@ module BlessThisRedmineSso
 
         # If SSO-only mode is enabled, ALL users must use external password management
         if settings['oauth_sso_only'] == '1' || settings['oauth_sso_only'] == true
-          casdoor_url = ENV['CASDOOR_EXTERNAL_URL'] || 'http://localhost:8082'
-
-          Rails.logger.info "SSO-only mode active, redirecting to #{casdoor_url}/account"
-
-          flash[:error] = "Password management and User Information is handled by your corporate SSO provider. Please visit your SSO provider to change them."
-          redirect_to "#{casdoor_url}/account" and return
+          Rails.logger.info "SSO-only mode active, redirecting to /my/account"
+          redirect_to my_account_path and return
         end
 
         # If SSO-only is disabled, check if current user is an OAuth user
         if User.current.respond_to?(:is_oauth_user?) && User.current.is_oauth_user?
-          casdoor_url = ENV['CASDOOR_EXTERNAL_URL'] || 'http://localhost:8082'
-
-          Rails.logger.info "OAuth user detected, redirecting to #{casdoor_url}/account"
-
-          flash[:error] = "Password management and User Information is handled by your corporate SSO provider. Please visit your SSO provider to change them."
-          redirect_to "#{casdoor_url}/account" and return
+          Rails.logger.info "OAuth user detected, redirecting to /my/account"
+          redirect_to my_account_path and return
         end
 
         Rails.logger.info "No redirect conditions met, calling super"
